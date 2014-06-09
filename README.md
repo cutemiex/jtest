a403b3713b559a758af413625bba603fa3a64e4c
tommy <cutemiex@gmail.com>
2014-06-09 14:14:55 +0800

    adding some implementation for concurrent

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the apare time small testing project.
+Used for prototype some small features.
--- src/test/java/com/doudou/tian/concurrent/CollaborativeTransferQueueTest.java
+++ src/test/java/com/doudou/tian/concurrent/CollaborativeTransferQueueTest.java
@@ -6,22 +6,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.Test;
+
+import com.doudou.tian.concurrent.lock.LatchDown;
 
 public class CollaborativeTransferQueueTest {
 
     CollaborativeTransferQueue<Integer> q = new CollaborativeTransferQueue<Integer>();
-    final CountDownLatch allExecutorThreadsReady = new CountDownLatch(10);
-    final CountDownLatch afterInitBlocker = new CountDownLatch(1);
-    final CountDownLatch allDone = new CountDownLatch(10);
+//	SynchronousQueue<Integer> q = new SynchronousQueue<Integer>();
+
+	final LatchDown allExecutorThreadsReady = new LatchDown(10);
+    final LatchDown afterInitBlocker = new LatchDown(1);
+    final LatchDown allDone = new LatchDown(10);
 
     @Test
-    public void assertConcurrent() throws InterruptedException {
+    public void assertConcurrent() throws InterruptedException, TimeoutException {
         final List<Throwable> exceptions = Collections.synchronizedList(new ArrayList<Throwable>());
 
         final ExecutorService threadPool = Executors.newFixedThreadPool(100);
@@ -32,12 +36,11 @@ public class CollaborativeTransferQueueTest {
                 threadPool.submit(new ThatRun(i));
             }
             // wait until all threads are ready
-            assertTrue(
-                    "Timeout initializing threads! Perform long lasting initializations before passing runnables to assertConcurrent",
-                    allExecutorThreadsReady.await(10, TimeUnit.MILLISECONDS));
+            allExecutorThreadsReady.await(10, TimeUnit.MILLISECONDS);
+            
             // start all test runners
             afterInitBlocker.countDown();
-            assertTrue(" timeout! More than" + 3 + "seconds", allDone.await(1000, TimeUnit.SECONDS));
+            allDone.await(300000, TimeUnit.SECONDS);
 
         } finally {
             threadPool.shutdownNow();
@@ -47,34 +50,34 @@ public class CollaborativeTransferQueueTest {
     }
 
     class ThatRun implements Runnable {
-        private int i;
+        private int mod;
 
-        public ThatRun(int i) {
-            this.i = i;
+        public ThatRun(int r) {
+            this.mod = r;
         }
 
         public void run() {
             allExecutorThreadsReady.countDown();
             try {
                 afterInitBlocker.await();
-                for (;;) {
-                    Integer r = new Random().nextInt(1000000);
-                    if (i % 2 == 0) {
+                for (int i =0; i< 5000 ; i++) {
+                    Integer r = new Random().nextInt(500);
+                    if (mod % 2 == 0) {
                         try {
                             Integer o = q.poll(3, TimeUnit.SECONDS);
-//                            System.out.println(Thread.currentThread().getId() + "Get value " + o);
+                            System.out.println(Thread.currentThread().getId() + "Get value " + o);
                         } catch (InterruptedException e) {
-//                            System.out.println(Thread.currentThread().getId() + "Timeout for retrive");
+                            System.out.println(Thread.currentThread().getId() + "Timeout for retrive");
                         }
                     } else {
                         try {
-//                            System.out.println(Thread.currentThread().getId() + "offer value " + r);
+                            System.out.println(Thread.currentThread().getId() + "offer value " + r);
                             q.offer(r, 3, TimeUnit.SECONDS);
                         } catch (InterruptedException e) {
-//                            System.out.println(Thread.currentThread().getId() + "TimeOUt for val" + r);
+                            System.out.println(Thread.currentThread().getId() + "TimeOUt for val" + r);
                         }
                     }
-                    Thread.sleep(5);
+//                    Thread.sleep(5);
                 }
             } catch (final Throwable e) {
                 System.out.println(Thread.currentThread().getId() + " is down");
