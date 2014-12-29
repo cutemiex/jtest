45570dd08fb4739c260cc860bf660f7f9df523e2
胡魁 <hukui@alibaba-inc.com>
2014-12-29 17:44:01 +0800

    delete it

--- src/main/resources/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Starting to run the command $*"
-
-java -cp $CLASSPATH:. -verbose:gc -XX:HeapDumpPath=./dump.hprof -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:./gc.log -XX:+PrintFlagsFinal -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Xms20M -Xmx20M -Xmn10M $@
